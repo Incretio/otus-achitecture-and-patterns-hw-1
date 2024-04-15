4a9a3f999cf629373ee8a18241b6f6425869347f
incretio <incretio@gmail.com>
2024-04-15 20:27:22 +0300

    init commit

---
+++ src/main/java/com/incretio/study/otus/architectureandpatterns/QuadraticEquation.java
@@ -0,0 +1,9 @@
+package com.incretio.study.otus.architectureandpatterns;
+
+public class QuadraticEquation {
+
+    public double solve() {
+        return 0d;
+    }
+
+}
---
+++ src/test/java/com/incretio/study/otus/architectureandpatterns/QuadraticEquationTest.java
@@ -0,0 +1,11 @@
+package com.incretio.study.otus.architectureandpatterns;
+
+import org.junit.jupiter.api.Test;
+
+class QuadraticEquationTest {
+
+    @Test
+    void solve() {
+    }
+
+}
